791887bccea1da57d51b80334c3fad2f0d839058
Fluvio <fluvio.lobo@gmail.com>
2016-02-29 23:23:09 -0500

    For this commit, most of the modifications consisted of comments on each important line of the code. Technical changes included changing variables from double to floats (higher precision) and adding the proper conversion factor for the mapped analog output to real voltages (in mV).

--- Software/analogAudio/analogAudio.ino
+++ Software/analogAudio/analogAudio.ino
@@ -1,26 +1,29 @@
-/* ==========================
+/* =====================================
  * Analog Audio Read
  * 
  * The following script was designed to test the capabilities of the SparkFun MEMs Microphone ADMP401
  * The script simply reads the output voltage from the MEMs microphone.
  * 
- * Fluvio L. Lobo Fenoglietto
- ========================= */
+ * Fluvio L. Lobo Fenoglietto 02/29/2016
+ ==================================== */
 
-int analogPin = 0;
-double val;
-double volts;
+
+// Variable definitions
+int analogPin = 0; //this variable indicates the analog pin used by the MEMS microphone
+int val; //this variable will be defined as the automatic mapping of the 3.3volts using integers between 0 and 1023
+float volts; //this variable will be defined as the actual voltage (in mV) of the analog signal
 
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(9600); //begin serial communication at 9600bps
 
-}
+} //End of void-setup
 
 void loop() {
 
-  val = analogRead(analogPin);
-  volts = val/1024;
-  Serial.println(volts);
+  val = analogRead(analogPin); //read the analog output --automatically mapped to integers between 0 and 1023
+  volts = val * (5.0 / 1023.0); //here the program determines the real voltage of the signal
+  Serial.println(volts); //prints the voltage to the serial monitor
 
-}
+} //End of void-loop
+
